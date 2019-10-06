490002b01a1d42ba6b8f711fde3a34115363caa4
Jason Hundal <jasonhundal23@gmail.com>
2019-10-06 21:08:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+
+def map(arr)
+  updated = []
+  counter = 0
+  while counter < arr.length
+    updated.push(yield(arr[counter]))
+    counter += 1
+  end
+  updated
+end
+
+def reduce(arr, arr1=nil)
+  if arr1
+    updated = arr1
+    counter = 0
+  else
+    counter = 1
+    updated = arr[0]
+  end
+  
+  while counter < arr.length
+    updated = yield(updated, arr[counter])
+    counter = counter + 1
+  end
+  
+  updated
+end
